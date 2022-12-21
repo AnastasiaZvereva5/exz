da1bc5793e53f8645df086676971a2e7b9c13626
Mamina pussy <efroceiraikovckie@gmal.com>
2022-12-21 11:51:45 +0300

    first commit

---
+++ Desktop_Auth/pythonProject/main.py
@@ -0,0 +1,42 @@
+from tkinter import Tk, Label, CENTER, messagebox, Entry, Button
+
+
+
+window = Tk()
+window.title('Авторизация')
+window.geometry('450x230')
+window.resizable(False, False)
+font_header = ('TimesNewRoman', 15)
+font_entry = ('TimesNewRoman', 12)
+label_font = ('TimesNewRoman', 11)
+base_padding = {'padx': 10, 'pady': 8}
+header_padding = {'padx': 10, 'pady': 12}
+def clicked():
+
+    username = username_entry.get()
+    password = password_entry.get()
+
+    messagebox.showinfo(
+    'Заголовок', '{username}, {password}'.format(username=username, password=password))
+
+
+main_label = Label(
+    window, text='Авторизация', font=font_header, justify=CENTER, **header_padding)
+main_label.pack()
+username_label = Label(
+    window, text='Имя пользователя', font=label_font , **base_padding)
+username_label.pack()
+username_entry = Entry(
+    window, bg='#fff', fg='#444', font=font_entry)
+username_entry.pack()
+password_label = Label(
+    window, text='Пароль', font=label_font , **base_padding)
+password_label.pack()
+password_entry = Entry(
+    window, bg='#fff', fg='#444', font=font_entry)
+password_entry.pack()
+send_btn = Button(
+    window, text='Войти', command=clicked)
+send_btn.pack(**base_padding)
+window.mainloop()
+
